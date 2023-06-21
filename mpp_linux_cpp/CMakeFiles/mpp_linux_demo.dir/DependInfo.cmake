fbd53c3cdac0d6a722c0f9c9b6ecb9847e741499
rk3588 <wunyje@gmail.com>
2023-06-21 17:10:37 +0800

    update

---
+++ mpp_linux_cpp/CMakeFiles/mpp_linux_demo.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/rpdzkj/Downloads/rk3588_dev/mpp_linux_cpp/rkdrm/bo.c" "/home/rpdzkj/Downloads/rk3588_dev/mpp_linux_cpp/CMakeFiles/mpp_linux_demo.dir/rkdrm/bo.c.o"
+  "/home/rpdzkj/Downloads/rk3588_dev/mpp_linux_cpp/rkdrm/dev.c" "/home/rpdzkj/Downloads/rk3588_dev/mpp_linux_cpp/CMakeFiles/mpp_linux_demo.dir/rkdrm/dev.c.o"
+  "/home/rpdzkj/Downloads/rk3588_dev/mpp_linux_cpp/rkdrm/modeset.c" "/home/rpdzkj/Downloads/rk3588_dev/mpp_linux_cpp/CMakeFiles/mpp_linux_demo.dir/rkdrm/modeset.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/rpdzkj/Downloads/mpp/inc"
+  "/home/rpdzkj/Downloads/mpp"
+  "/home/rpdzkj/Downloads/mpp/osal/linux"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/rpdzkj/Downloads/rk3588_dev/mpp_linux_cpp/main.cpp" "/home/rpdzkj/Downloads/rk3588_dev/mpp_linux_cpp/CMakeFiles/mpp_linux_demo.dir/main.cpp.o"
+  "/home/rpdzkj/Downloads/rk3588_dev/mpp_linux_cpp/mpp/Codec.cpp" "/home/rpdzkj/Downloads/rk3588_dev/mpp_linux_cpp/CMakeFiles/mpp_linux_demo.dir/mpp/Codec.cpp.o"
+  "/home/rpdzkj/Downloads/rk3588_dev/mpp_linux_cpp/rkrga/RGA.cpp" "/home/rpdzkj/Downloads/rk3588_dev/mpp_linux_cpp/CMakeFiles/mpp_linux_demo.dir/rkrga/RGA.cpp.o"
+  "/home/rpdzkj/Downloads/rk3588_dev/mpp_linux_cpp/thread/Thread.cpp" "/home/rpdzkj/Downloads/rk3588_dev/mpp_linux_cpp/CMakeFiles/mpp_linux_demo.dir/thread/Thread.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/rpdzkj/Downloads/mpp/inc"
+  "/home/rpdzkj/Downloads/mpp"
+  "/home/rpdzkj/Downloads/mpp/osal/linux"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ mpp_linux_cpp/CMakeFiles/mpp_linux_demo.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mpp_linux_demo.dir/main.cpp.o"
+  "CMakeFiles/mpp_linux_demo.dir/mpp/Codec.cpp.o"
+  "CMakeFiles/mpp_linux_demo.dir/rkdrm/bo.c.o"
+  "CMakeFiles/mpp_linux_demo.dir/rkdrm/dev.c.o"
+  "CMakeFiles/mpp_linux_demo.dir/rkdrm/modeset.c.o"
+  "CMakeFiles/mpp_linux_demo.dir/rkrga/RGA.cpp.o"
+  "CMakeFiles/mpp_linux_demo.dir/thread/Thread.cpp.o"
+  "mpp_linux_demo"
+  "mpp_linux_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/mpp_linux_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
