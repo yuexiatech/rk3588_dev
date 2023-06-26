ff2a5f4a04d9aa534761082c8a46404893e12cf3
WT <244001428@qq.com>
2023-06-26 09:44:57 +0800

    update

--- shell_cmd/gst-cmd.sh
+++ shell_cmd/gst-cmd.sh
@@ -6,4 +6,5 @@ gst-launch-1.0 -v v4l2src device=/dev/video31 ! video/x-raw,width=640,height=480
 # camera video  recieve
 gst-launch-1.0 -v udpsrc port=5000 ! application/x-rtp, encoding-name=H264,payload=96 ! rtph264depay ! avdec_h264 ! videoconvert ! autovideosink
 
-
+# format transforming
+gst-launch-1.0 filesrc location=Tennis1080p.h264 ! video/x-h264 ! h264parse ! mp4mux ! filesink location=Tennis1080p.mp4
