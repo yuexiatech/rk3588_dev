9e305ca9cd28c47694ed0e6f60cfa24d4b59c777
WT <244001428@qq.com>
2023-06-28 15:05:19 +0800

    update

--- shell_cmd/gst-cmd.sh
+++ shell_cmd/gst-cmd.sh
@@ -55,3 +55,16 @@ gst-launch-1.0 filesrc location=Tennis1080p.mp4 ! decodebin name=decode ! tee na
 gst-launch-1.0 -v filesrc location=Tennis1080p.mp4 ! decodebin ! x264enc tune=zerolatency bitrate=1000 ! rtph264pay ! udpsink host=192.168.8.90 port=5000 qos=true
 
 gst-launch-1.0 -v udpsrc port=5000 caps = "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! decodebin ! videoconvert ! autovideosink qos=true
+    
+# multi display
+gst-launch-1.0 -ev \
+videomixer name=mix \
+sink_0::xpos=0 sink_0::ypos=0 sink_0::alpha=1 \
+sink_1::xpos=320 sink_1::ypos=0 sink_1::alpha=1 \
+sink_2::xpos=0 sink_2::ypos=240 sink_2::alpha=1 \
+sink_3::xpos=320 sink_3::ypos=240 sink_3::alpha=1 \
+! autovideosink qos=true \
+uridecodebin uri=http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4 ! decodebin ! videoscale ! video/x-raw,width=320,height=240 ! mix.sink_0 \
+uridecodebin uri=http://vjs.zencdn.net/v/oceans.mp4 ! decodebin ! videoscale ! video/x-raw,width=320,height=240 ! mix.sink_1 \
+uridecodebin uri=https://media.w3.org/2010/05/sintel/trailer.mp4 ! decodebin ! videoscale ! video/x-raw,width=320,height=240 ! mix.sink_2 \
+uridecodebin uri=http://mirror.aarnet.edu.au/pub/TED-talks/911Mothers_2010W-480p.mp4 ! decodebin ! videoscale ! video/x-raw,width=320,height=240 ! mix.sink_3
