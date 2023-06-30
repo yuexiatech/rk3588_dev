05e24a89b9976bcbe1585d83ae790589b6837c71
WT <244001428@qq.com>
2023-06-30 11:13:36 +0800

    update

--- gst_dev/my_double_camera.c
+++ gst_dev/my_double_camera.c
@@ -15,65 +15,94 @@ v4l2src device=/dev/video41 ! image/jpeg,width=320,height=240,framerate=30/1 ! j
 
 // gcc my_double_camera.c -o my_double_camera ` pkg-config --cflags --libs gstreamer-1.0`
 
-int
-main (int argc, char *argv[])
-{
-    // Create a pipeline and a videomixer element
-    GstElement *pipeline = gst_pipeline_new ("pipeline");
-    GstElement *mixer = gst_element_factory_make ("videomixer", "mix");
-    GstBus *bus;
-    GstMessage *msg;
+int main(int argc, char *argv[]) {
+  GstElement *pipeline, *mixer, *sink, *source1, *convert1, *scale1, *capsfilter1, *source2, *capsfilter2, *decoder;
+  GstCaps *caps1, *caps2;
+  GstBus *bus;
+  GstMessage *msg;
 
-    // Set the properties of the mixer
-    g_object_set (mixer, "sink_0::xpos", 0, "sink_0::ypos", 0, "sink_0::alpha", 1, NULL);
-    g_object_set (mixer, "sink_1::xpos", 0, "sink_1::ypos", 240, "sink_1::alpha", 1, NULL);
+  /* Initialize GStreamer */
+  gst_init (&argc, &argv);
 
-    // Add the mixer to the pipeline
-    gst_bin_add (GST_BIN (pipeline), mixer);
+  /* Create the elements */
+  mixer = gst_element_factory_make ("videomixer", "mixer");
+  sink = gst_element_factory_make ("autovideosink", "sink");
+  source1 = gst_element_factory_make ("v4l2src", "source1");
+  convert1 = gst_element_factory_make ("videoconvert", "convert1");
+  scale1 = gst_element_factory_make ("videoscale", "scale1");
+  capsfilter1 = gst_element_factory_make ("capsfilter", "capsfilter1");
+  source2 = gst_element_factory_make ("v4l2src", "source2");
+  capsfilter2 = gst_element_factory_make ("capsfilter", "capsfilter2");
+  decoder = gst_element_factory_make ("jpegdec", "decoder");
 
-    // Create and add the first video source
-    GstElement *src1 = gst_element_factory_make ("v4l2src", "src1");
-    g_object_set (src1, "device", "/dev/video31", NULL);
-    GstElement *convert = gst_element_factory_make ("videoconvert", "convert");
-    GstElement *scale = gst_element_factory_make ("videoscale", "scale");
-    GstCaps *caps1 = gst_caps_from_string ("video/x-raw, width=320, height=240");
-    gst_bin_add_many (GST_BIN (pipeline), src1, convert, scale, NULL);
-    gst_element_link_filtered (src1, convert, caps1);
-    gst_element_link (convert, scale);
+  /* Create the empty pipeline */
+  pipeline = gst_pipeline_new ("test-pipeline");
 
-    // Create and add the second video source
-    GstElement *src2 = gst_element_factory_make ("v4l2src", "src2");
-    g_object_set (src2, "device", "/dev/video41", NULL);
-    GstElement *dec2 = gst_element_factory_make ("jpegdec", "dec2");
-    GstCaps *caps2 = gst_caps_from_string ("image/jpeg,width=320,height=240,framerate=30/1");
-    gst_bin_add_many (GST_BIN (pipeline), src2, dec2, NULL);
-    gst_element_link_filtered (src2, dec2, caps2);
+  if (!pipeline || !mixer || !sink || !source1 || !convert1 || !scale1 || !capsfilter1 || !source2 || !capsfilter2 || !decoder) {
+    g_printerr ("Not all elements could be created.\n");
+    return -1;
+  }
 
-    // Link the sources to the mixer
-    GstPad *pad1 = gst_element_get_request_pad (mixer, "sink_%u");
-    GstPad *pad2 = gst_element_get_request_pad (mixer, "sink_%u");
-    gst_element_link_pads (scale, "src1", mixer, GST_OBJECT_NAME (pad1));
-    gst_element_link_pads (dec2, "src2", mixer, GST_OBJECT_NAME (pad2));
+  /* Set the device property on the v4l2src elements */
+  g_object_set (G_OBJECT (source1), "device", "/dev/video31", NULL);
+  g_object_set (G_OBJECT (source2), "device", "/dev/video41", NULL);
 
-    // Create and add the video sink
-    GstElement *sink = gst_element_factory_make ("autovideosink", "sink");
-    g_object_set (sink, "qos", TRUE, NULL);
-    gst_bin_add (GST_BIN (pipeline), sink);
-    gst_element_link (mixer, sink);
+  /* Set the caps on the capsfilter elements */
+  caps1 = gst_caps_from_string ("video/x-raw, width=320, height=240");
+  g_object_set (G_OBJECT (capsfilter1), "caps", caps1, NULL);
+  gst_caps_unref (caps1);
 
-    // Start playing the pipeline
-    gst_element_set_state (pipeline, GST_STATE_PLAYING);
+  caps2 = gst_caps_from_string ("image/jpeg,width=320,height=240,framerate=30/1");
+  g_object_set (G_OBJECT (capsfilter2), "caps", caps2, NULL);
+  gst_caps_unref (caps2);
 
-    /* Wait until error or EOS */
-    bus = gst_element_get_bus (pipeline);
-    msg = gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE,
-        GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+  /* Set properties on the mixer pads */
+  GstPad *mixer_sink_pad_0 = gst_element_get_request_pad(mixer, "sink_%u");
+  g_object_set(mixer_sink_pad_0, "xpos", 0, "ypos", 0, "alpha", (gdouble)1.0, NULL);
 
-    /* Free resources */
-    if (msg != NULL)
-        gst_message_unref (msg);
-    gst_object_unref (bus);
-    gst_element_set_state (pipeline, GST_STATE_NULL);
-    gst_object_unref (pipeline);
+  GstPad *mixer_sink_pad_1 = gst_element_get_request_pad(mixer, "sink_%u");
+  g_object_set(mixer_sink_pad_1, "xpos", 0, "ypos", 240, "alpha", (gdouble)1.0, NULL);
+
+  /* Build the pipeline */
+  gst_bin_add_many (GST_BIN (pipeline), mixer, sink, source1, convert1, scale1,
+      capsfilter1, source2, capsfilter2, decoder, NULL);
+  
+   if (!gst_element_link_many(source1, convert1, scale1,
+      capsfilter1,NULL)) {
+    g_printerr("Failed to link source #1\n");
+    return -1;
+   }
+   if (!gst_element_link_pads(capsfilter1,"src",mixer_sink_pad_0,"sink")) {
+    g_printerr("Failed to link mixer pad #0\n");
+    return -1;
+   }
+   if (!gst_element_link_many(source2,capsfilter2,
+      decoder,NULL)) {
+    g_printerr("Failed to link source #2\n");
+    return -1;
+   }
+   if (!gst_element_link_pads(decoder,"src",mixer_sink_pad_1,"sink")) {
+    g_printerr("Failed to link mixer pad #0\n");
+    return -1;
+   }
+   if (!gst_element_link_many(mixer,sink,NULL)) {
+    g_printerr("Failed to link mixer and sink\n");
+    return -1;
+   }
+
+   /* Start playing */
+   gst_element_set_state(pipeline,GST_STATE_PLAYING);
+
+   /* Wait until error or EOS */
+   bus = gst_element_get_bus(pipeline);
+   msg = gst_bus_timed_pop_filtered(bus,GST_CLOCK_TIME_NONE,
+       GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+
+   /* Free resources */
+   if(msg != NULL)
+     gst_message_unref(msg);
+   gst_object_unref(bus);
+   gst_element_set_state(pipeline,GST_STATE_NULL);
+   gst_object_unref(pipeline);
+   return 0;
 }
-
