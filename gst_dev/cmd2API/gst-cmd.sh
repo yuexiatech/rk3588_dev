c6e22d01717a1b532157c23d8c0f40854b1fad5f
Wunyje <wunyje@gmail.com>
2023-07-07 11:23:37 +0800

    update

--- gst_dev/cmd2API/gst-cmd.sh
+++ gst_dev/cmd2API/gst-cmd.sh
@@ -109,3 +109,10 @@ gst-launch-1.0 -e \
     v4l2src device=/dev/video31 ! video/x-raw,format=NV12,width=800,height=600,framerate=30/1 ! videoconvert ! queue ! videomixer name=mix sink_0::xpos=0 sink_0::ypos=0 sink_1::xpos=800 sink_1::ypos=0 ! x264enc ! mp4mux ! filesink location=/tmp/output.mp4 \
     v4l2src device=/dev/video41 ! image/jpeg,width=640,height=480,framerate=30/1 ! jpegdec ! videoscale ! video/x-raw,width=800,height=600 ! queue ! mix.
 
+gst-launch-1.0  \
+videomixer name=mix \
+sink_0::xpos=0 sink_0::ypos=0 sink_0::alpha=1 \
+sink_1::xpos=0 sink_1::ypos=240 sink_1::alpha=1 \
+! x264enc ! mp4mux ! filesink location=/tmp/output.mp4 \
+v4l2src device=/dev/video31 ! videoconvert! videoscale ! video/x-raw, width=320, height=240 ! mix.sink_0 \
+v4l2src device=/dev/video41 ! image/jpeg,width=320,height=240,framerate=30/1 ! jpegdec ! mix.sink_1
