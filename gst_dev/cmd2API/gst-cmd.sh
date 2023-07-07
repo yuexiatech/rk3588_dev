26a8abcf7a2499d6f711ceb24c82584809ee0246
WT <244001428@qq.com>
2023-07-07 11:11:00 +0800

    upload mp4 files

--- gst_dev/cmd2API/gst-cmd.sh
+++ gst_dev/cmd2API/gst-cmd.sh
@@ -106,6 +106,6 @@ v4l2src device=/dev/video31 ! videoconvert! videoscale ! video/x-raw, width=320,
 v4l2src device=/dev/video41 ! image/jpeg,width=320,height=240,framerate=30/1 ! jpegdec ! mix.sink_1
 
 gst-launch-1.0 -e \
-    v4l2src device=/dev/video31 num-buffers=100 ! video/x-raw,format=NV12,width=800,height=600,framerate=30/1 ! videoconvert ! queue ! videomixer name=mix sink_0::xpos=0 sink_0::ypos=0 sink_1::xpos=800 sink_1::ypos=0 ! x264enc ! mp4mux ! filesink location=/tmp/output.mp4 \
+    v4l2src device=/dev/video31 ! video/x-raw,format=NV12,width=800,height=600,framerate=30/1 ! videoconvert ! queue ! videomixer name=mix sink_0::xpos=0 sink_0::ypos=0 sink_1::xpos=800 sink_1::ypos=0 ! x264enc ! mp4mux ! filesink location=/tmp/output.mp4 \
     v4l2src device=/dev/video41 ! image/jpeg,width=640,height=480,framerate=30/1 ! jpegdec ! videoscale ! video/x-raw,width=800,height=600 ! queue ! mix.
 
