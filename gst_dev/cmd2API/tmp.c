006ba5a0146852af2767b410eb7cec9e42425567
WT <244001428@qq.com>
2023-07-10 10:23:52 +0800

    update

--- gst_dev/cmd2API/tmp.c
+++ gst_dev/cmd2API/tmp.c
@@ -78,28 +78,4 @@ if(msg!=NULL)
     GError*err;
     gchar*debug_info;
 
-    switch(GST_MESSAGE_TYPE(msg))
-    {
-        case GST_MESSAGE_ERROR:
-            gst_message_parse_error(msg,&err,&debug_info);
-            g_printerr("Error received from element %s: %s\n",GST_OBJECT_NAME(msg->src),err->message);
-            g_printerr("Debugging information: %s\n",(debug_info)?debug_info:"none");
-            g_clear_error(&err);
-            g_free(debug_info);
-            break;
-        case GST_MESSAGE_EOS:
-            g_print("End-Of-Stream reached.\n");
-            break;
-        default:
-            g_printerr("Unexpected message received.\n");
-            break;
-    }
-    gst_message_unref(msg);
-}
-
-gst_object_unref(bus);
-gst_element_set_state(pipeline,GST_STATE_NULL);
-gst_object_unref(pipeline);
-
-return 0;
-}
+    switch(G
