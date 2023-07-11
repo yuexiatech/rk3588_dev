fc5db16d93f2ab909d16c7b26d3258ffe613aeec
WT <244001428@qq.com>
2023-07-11 09:15:55 +0800

    update

--- gst_dev/cmd2API/my-camera-confluence-ez.c
+++ gst_dev/cmd2API/my-camera-confluence-ez.c
@@ -9,8 +9,7 @@ int main(int argc, char *argv[]) {
   gst_init (&argc, &argv);
 
   /* Build the pipeline */
-  pipeline = gst_parse_launch ("gst-launch-1.0 -e \
-    v4l2src name=src1 device=/dev/video31 ! video/x-raw,format=NV12,width=800,height=600,framerate=30/1 ! videoconvert ! queue ! videomixer name=mix sink_0::xpos=0 sink_0::ypos=0 sink_1::xpos=800 sink_1::ypos=0 ! x264enc ! mp4mux name=mux ! filesink location=/tmp/output.mp4 \
+  pipeline = gst_parse_launch ("v4l2src name=src1 device=/dev/video31 ! video/x-raw,format=NV12,width=800,height=600,framerate=30/1 ! videoconvert ! queue ! videomixer name=mix sink_0::xpos=0 sink_0::ypos=0 sink_1::xpos=800 sink_1::ypos=0 ! x264enc ! mp4mux name=mux ! filesink location=/tmp/output.mp4 \
     v4l2src name=src2 device=/dev/video32 ! video/x-raw,format=NV12,width=800,height=600,framerate=30/1 ! videoconvert ! queue ! mix.", NULL);
 
   /* Set group-id property of v4l2src and videomixer elements */
