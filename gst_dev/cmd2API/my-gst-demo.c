ca78e35fa941a565df248a79fd80b735a2a91f69
WT <244001428@qq.com>
2023-07-11 16:44:34 +0800

    update

--- gst_dev/cmd2API/my-gst-demo.c
+++ gst_dev/cmd2API/my-gst-demo.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]) {
   gst_init (&argc, &argv);
 
   /* Build the pipeline */
-  pipeline = gst_parse_launch ("v4l2src device=/dev/video31 num-buffers=100 ! video/x-raw,format=NV12,width=800,height=600,framerate=30/1 ! videoconvert ! queue ! videomixer name=mix sink_0::xpos=0 sink_0::ypos=0 sink_1::xpos=800 sink_1::ypos=0 ! x264enc ! mp4mux ! filesink location=/tmp/output.mp4 \
+  pipeline = gst_parse_launch ("v4l2src device=/dev/video31 num-buffers=100 ! video/x-raw,format=NV12,width=800,height=600,framerate=30/1 ! videoconvert ! queue ! videomixer name=mix sink_0::xpos=0 sink_0::ypos=0 sink_1::xpos=800 sink_1::ypos=0 ! mpph264enc ! h264parse ! mp4mux ! filesink location=/tmp/output.mp4 \
     v4l2src device=/dev/video41 num-buffers=100 ! image/jpeg,width=800,height=600,framerate=30/1 ! jpegdec ! queue ! mix.", &error);
 
   if (!pipeline) {
